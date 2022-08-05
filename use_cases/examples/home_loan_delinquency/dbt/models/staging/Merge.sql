5546ef95cfd51906fa9ee774701ca9381d319720
harshMach1ne <110524582+harshMach1ne@users.noreply.github.com>
2022-08-05 10:58:12 +0530

    Update Merge.sql

--- use_cases/examples/home_loan_delinquency/dbt/models/staging/Merge.sql
+++ use_cases/examples/home_loan_delinquency/dbt/models/staging/Merge.sql
@@ -1,9 +1,6 @@
 SELECT
 region_codes.REGION AS Region_Name,
 property.PROPERTY_TYPE AS Type_of_Property
-SUM(property.SECURITY_VALUE) AS Security_Value
 FROM 
 {{ref('ref_region_codes')}} as region_codes
 inner join {{ref('src_securities')}} as property on region_codes.PROPERTY_POST_CODE=property.PROPERTY_POST_CODE
-GROUP BY REGION_Name, Type_of_Property
-ORDER BY Number_of_Properties;
