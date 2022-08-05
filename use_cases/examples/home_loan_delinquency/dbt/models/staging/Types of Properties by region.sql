735b3f0b6be9639b373a84d2c526c025b0288369
harshMach1ne <110524582+harshMach1ne@users.noreply.github.com>
2022-08-05 10:17:00 +0530

    Update Types of Properties by region.sql

--- use_cases/examples/home_loan_delinquency/dbt/models/staging/Types of Properties by region.sql
+++ use_cases/examples/home_loan_delinquency/dbt/models/staging/Types of Properties by region.sql
@@ -1,7 +1,7 @@
 SELECT
 region_codes.REGION AS Region_Name,
 property.PROPERTY_TYPE AS Type_of_Property
-COUNT(property.PROPERTY_TYPE) AS Number_of_Properties
+SUM(property.SECURITY_VALUE) AS Security_Value
 FROM 
 {{ref('ref_region_codes')}} as region_codes
 inner join {{ref('src_securities')}} as property on region_codes.PROPERTY_POST_CODE=property.PROPERTY_POST_CODE
